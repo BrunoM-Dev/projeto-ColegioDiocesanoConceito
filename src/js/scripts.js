3d14085e7f69139e534d2ca67d90495b1968ca2a
Bruno Macedo <devbrunomacedo@gmail.com>
2023-12-07 15:29:31 -0300

    funcionalidade javascript

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -24,3 +24,28 @@ function menuHidden(){
 }
 
 window.addEventListener('scroll', menuHidden);
+
+// Função para verificar se um elemento está visível na janela de visualização
+function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function handleScroll() {
+    var meuElemento = document.getElementById('titulo-maps');
+
+    if (isElementInViewport(meuElemento)) {
+        meuElemento.classList.add('typing-animation');
+    } else {
+        meuElemento.classList.remove('typing-animation');
+    }
+}
+
+window.addEventListener('scroll', handleScroll);
+
+handleScroll();
